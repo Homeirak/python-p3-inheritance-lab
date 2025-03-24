3c764c833ef9912ffd393535cd6281b9ccc4e69d
homeira kalim <homeirakalim@gmail.com>
2025-03-24 03:48:10 -0400

    python object inheritance lab 1 complete

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+# student.py
 
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, string):
+        self.knowledge.append(string)
